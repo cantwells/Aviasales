392e4a0abf0a48a3709aa65c7c3e15b2aed10dde
cantwell <rjazantsev@gmail.com>
2020-03-19 11:39:20 +0300

    First Stage. selections of cities for input

---
+++ main.js
@@ -0,0 +1,59 @@
+//БД городов
+const citiesList = ['Москва', 'Санкт-Петербург', 'Симферополь', 'Казань', 'Екатеринбург', 'Краснодар',
+    'Минск', 'Киев', 'Владивосток', 'Нижний Новгород', 'Ростов-на-Дону', 'Челябинск', 'Новосибирск',
+    'Самара', 'Томбов', 'Архангельск', 'Тверь', 'Рига', 'Таллин', 'Калининград', 'Вильнюс', 'Тбилиси',
+    'Ереван', 'Баку', 'Астана', 'Ташкент', 'Бишкек', 'Душанбе', 'Лондон', 'Вашингтон', 'Берлин', 'Рим'
+];
+
+//Получаем элементы со страницы
+const inputCitiesFrom = document.querySelector('.input__cities-from'),
+    dropDownCitiesFrom = document.querySelector('.dropdown__cities-from'),
+    inputCitiesTo = document.querySelector('.input__cities-to'),
+    dropDownCitiesTo = document.querySelector('.dropdown__cities-to'),
+    buttonSearch = document.querySelector('.button__search');
+
+//Функции
+
+//Функция получения совпадающий с запросом городов
+const getCitiesList = (list, input) => {
+    return list.filter(function(item) {
+        item = item.toLowerCase();
+        if (input.value !== '')
+            return item.startsWith(input.value.toLowerCase());
+    });
+};
+
+const createLi = text => {
+    let liElem = document.createElement('li');
+    liElem.classList.add('dropdown__city');
+    liElem.textContent = text;
+    return liElem;
+}
+
+inputCitiesFrom.addEventListener('input', () => {
+    dropDownCitiesFrom.textContent = '';
+    const citiesMatchList = getCitiesList(citiesList, inputCitiesFrom);
+
+    citiesMatchList.forEach((item) => {
+        const li = createLi(item);
+        dropDownCitiesFrom.append(li);
+        li.addEventListener('click', function() {
+            inputCitiesFrom.value = this.textContent;
+            dropDownCitiesFrom.textContent = '';
+        });
+    });
+});
+
+inputCitiesTo.addEventListener('input', () => {
+    dropDownCitiesTo.textContent = '';
+    const citiesMatchList = getCitiesList(citiesList, inputCitiesTo);
+
+    citiesMatchList.forEach((item) => {
+        const li = createLi(item);
+        dropDownCitiesTo.append(li);
+        li.addEventListener('click', function() {
+            inputCitiesTo.value = this.textContent;
+            dropDownCitiesTo.textContent = '';
+        });
+    });
+});
